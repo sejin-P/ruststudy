49b5f7a90e472cb278012876dd8882cc29e70f42
sejin-P <chunpark37@gmail.com>
2023-04-22 01:28:08 +0900

    syntax practice

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    // str = immutable reference to utf-8 encoded string data stored.
+    let s1: &str = "World";
+    println!("s1: {s1}");
+
+    // String = wrapper around a vector of bytes.
+    let mut s2: String = String::from("Hello ");
+    println!("s2: {s2}");
+    s2.push_str(s1);
+    println!("s2: {s2}");
+
+    let s3: &str = &s2[6..];
+    println!("s3 {s3}");
+
+    // result
+    // s1: World
+    // s2: Hello
+    // s2: Hello World
+    // s3 World
 }
