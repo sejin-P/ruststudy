964ea12518182858effab483971b0aac67ce444f
sejin-P <chunpark37@gmail.com>
2023-05-02 00:49:20 +0900

    10.1 Move semantics

--- _10_ownership/src/main.rs
+++ _10_ownership/src/main.rs
@@ -10,5 +10,23 @@ fn main() {
         println!("x: {}", p.0);
     }
 
-    println!("y: {}", p.1);
+    // println!("y: {}", p.1);
+
+
+
+
+
+
+
+    // The assignment of s1 to s2 transfers ownership.
+    // The data was moved from s1 and s1 is no longer accessible.
+    // When s1 goes out of scope, nothing happens: it has no ownership.
+    // When s2 goes out of scope, the string data is freed.
+    // There is always exactly one variable binding which owns a value.
+    let s1: String = String::from("Hello!");
+    let s2: String = s1;
+    println!("s2: {s2}");
+    // println!("s1: {s1}");
+
+
 }
