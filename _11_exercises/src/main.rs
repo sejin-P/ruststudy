0ffa7bbb9ff5087c802511092857306256be9d89
sejin-P <chunpark37@gmail.com>
2023-06-07 00:25:59 +0900

    11.1 Designing a library

--- _11_exercises/src/main.rs
+++ _11_exercises/src/main.rs
@@ -25,28 +25,36 @@ impl Book {
 // - `self` for unique access by value.
 impl Library {
     fn new() -> Library {
-        todo!("Initialize and return a `Library` value")
+        Library {
+            books: { Vec::new() },
+        }
     }
 
-    //fn len(self) -> usize {
-    //    todo!("Return the length of `self.books`")
-    //}
+    fn len(&self) -> usize {
+       return self.books.len();
+    }
 
-    //fn is_empty(self) -> bool {
-    //    todo!("Return `true` if `self.books` is empty")
-    //}
+    fn is_empty(&self) -> bool {
+       return self.books.len() == 0;
+    }
 
-    //fn add_book(self, book: Book) {
-    //    todo!("Add a new book to `self.books`")
-    //}
+    fn add_book(&mut self, book: Book) {
+       self.books.push(book)
+    }
 
-    //fn print_books(self) {
-    //    todo!("Iterate over `self.books` and each book's title and year")
-    //}
+    fn print_books(&self) {
+       for book in &self.books {
+           println!("Title is {0}, year is {1}", book.title, book.year)
+       }
+    }
 
-    //fn oldest_book(self) -> Option<&Book> {
-    //    todo!("Return a reference to the oldest book (if any)")
-    //}
+    fn oldest_book(&self) -> Option<&Book> {
+       if self.books.len() != 0 {
+           return Some(&self.books[0]);
+       }
+
+        return None;
+    }
 }
 
 // This shows the desired behavior. Uncomment the code below and
@@ -54,23 +62,33 @@ impl Library {
 // method signatures, including the "self" parameter! You may
 // also need to update the variable bindings within main.
 fn main() {
-    let library = Library::new();
+    let mut library = Library::new();
 
-    //println!("The library is empty: {}", library.is_empty());
-    //
-    //library.add_book(Book::new("Lord of the Rings", 1954));
-    //library.add_book(Book::new("Alice's Adventures in Wonderland", 1865));
-    //
-    //println!("The library is no longer empty: {}", library.is_empty());
-    //
-    //
-    //library.print_books();
-    //
-    //match library.oldest_book() {
-    //    Some(book) => println!("The oldest book is {}", book.title),
-    //    None => println!("The library is empty!"),
-    //}
-    //
-    //println!("The library has {} books", library.len());
-    //library.print_books();
+    println!("The library is empty: {}", library.is_empty());
+
+    library.add_book(Book::new("Lord of the Rings", 1954));
+    library.add_book(Book::new("Alice's Adventures in Wonderland", 1865));
+
+    println!("The library is no longer empty: {}", library.is_empty());
+
+
+    library.print_books();
+
+    match library.oldest_book() {
+       Some(book) => println!("The oldest book is {}", book.title),
+       None => println!("The library is empty!"),
+    }
+
+    println!("The library has {} books", library.len());
+    library.print_books();
 }
+
+
+
+// I
+// Cannot
+// understand
+// how rust's self syntax works!!
+
+
+
