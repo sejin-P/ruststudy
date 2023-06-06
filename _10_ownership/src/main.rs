514d56f78b8bed8420de428026d01bff69c6d2f6
sejin-P <chunpark37@gmail.com>
2023-06-06 22:39:58 +0900

    10.8 Lifetimes in data structures

--- _10_ownership/src/main.rs
+++ _10_ownership/src/main.rs
@@ -57,6 +57,7 @@ fn main() {
     // cap 4
     let s1: String = String::from("Rust");
     let s2: String = s1;
+    // println!("s1: {s1}");
 
 
 
@@ -77,6 +78,7 @@ fn main() {
 
     let name = String::from("Alice");
     say_hello(name);
+    // println!("{name}");
 
 
 
@@ -214,4 +216,36 @@ fn main() {
     // In this case, p1 and p2 live at least as long as point, so there is no problem.
 
 
+
+
+
+
+
+
+
+
+
+
+
+    // 10.8 Lifetimes in data structures
+
+    #[derive(Debug)]
+    struct Highlight<'doc>(&'doc str);
+
+    fn erase(text: String) {
+        println!("Bye {text}!");
+    }
+
+    let text = String::from("The quick brown fox jumps over the lazy dog.");
+    let fox = Highlight(&text[4..19]);
+    let dog = Highlight(&text[35..43]);
+    // erase(text);
+    println!("{fox:?}");
+    println!("{dog:?}");
+
+    //     In the above example, the annotation on Highlight enforces that the data underlying the contained &str lives at least as long as any instance of Highlight that uses that data.
+    // If text is consumed before the end of the lifetime of fox (or dog), the borrow checker throws an error.
+    // Types with borrowed data force users to hold on to the original data. This can be useful for creating lightweight views, but it generally makes them somewhat harder to use.
+    // When possible, make data structures own their data directly.
+    // Some structs with multiple references inside can have more than one lifetime annotation. This can be necessary if there is a need to describe lifetime relationships between the references themselves, in addition to the lifetime of the struct itself. Those are very advanced use cases.
 }
