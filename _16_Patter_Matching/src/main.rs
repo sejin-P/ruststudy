91b91558d92119f00301fe2beac74e6c4e135da1
sejin-P <chunpark37@gmail.com>
2023-06-11 21:52:24 +0900

    16.4 Match Guards

--- _16_Patter_Matching/src/main.rs
+++ _16_Patter_Matching/src/main.rs
@@ -79,4 +79,25 @@ fn main() {
         [1, ..]   => println!("First is 1 and the rest were ignored"),
         _         => println!("All elements were ignored"),
     }
+
+
+
+
+
+
+
+
+
+
+
+
+    // 16.4 Match Guards
+    let pair = (0, 0);
+    println!("Tell me about {pair:?}");
+    match pair {
+        (x, y) if x == y     => println!("These are twins"),
+        (x, y) if x + y == 0 => println!("Antimatter, kaboom!"),
+        (x, _) if x % 2 == 1 => println!("The first one is odd"),
+        _                    => println!("No correlation..."),
+    }
 }
