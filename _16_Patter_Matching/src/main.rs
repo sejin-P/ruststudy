8a72d5db0ba8b6319d32abeec5090ed45425b2ea
sejin-P <chunpark37@gmail.com>
2023-06-11 21:49:11 +0900

    16.3 Destructing Arrays

--- _16_Patter_Matching/src/main.rs
+++ _16_Patter_Matching/src/main.rs
@@ -61,4 +61,22 @@ fn main() {
         Foo { y: 2, x: i} => println!("y = 2, x = {i:?}"),
         Foo { y, ..} => println!("y={y}, other fields are ignored")
     }
+
+
+
+
+
+
+
+
+
+
+    // 16.3 Destructing Arrays
+    let triple = [0, -2, 3];
+    println!("Tell me about {triple:?}");
+    match triple {
+        [0, y, z] => println!("First is 0, y = {y}, and z = {z}"),
+        [1, ..]   => println!("First is 1 and the rest were ignored"),
+        _         => println!("All elements were ignored"),
+    }
 }
