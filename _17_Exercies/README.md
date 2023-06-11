5f3e07eb6f6d79cb61edd2829347b99e662436a7
sejin-P <chunpark37@gmail.com>
2023-06-11 22:25:22 +0900

    confusing thing - continued 17.1

---
+++ _17_Exercies/README.md
@@ -0,0 +1,3 @@
+# confusing things
+
+you should assign struct value as mutable if you want to make that struct's field mutable too.
--- _17_Exercies/src/main.rs
+++ _17_Exercies/src/main.rs
@@ -30,8 +30,8 @@ impl User {
     }
 
     // copy를 구현하지 않았기 때문에 borrowing만 가능.
-    pub fn person(&self) -> &Person {
-        return &self.person;
+    pub fn person(&mut self) -> &mut Person {
+        return &mut self.person;
     }
 
     pub fn set_age(&mut self, new_age: u32) {
@@ -44,8 +44,10 @@ impl User {
 }
 
 fn main() {
-    let bob = User::new(String::from("Bob"), 32, 155.2);
+    let mut bob = User::new(String::from("Bob"), 32, 155.2);
     let mut age = bob.age();
+    let p = bob.person();
+    p.age = 5;
     age = 15;
     println!("I'm {} and my age is {}", bob.name(), bob.age());
 }
@@ -63,3 +65,108 @@ fn test_set_age() {
     bob.set_age(33);
     assert_eq!(bob.age(), 33);
 }
+
+
+
+
+pub struct Point {
+    // add fields
+}
+
+impl Point {
+    // add methods
+}
+
+pub struct Polygon {
+    // add fields
+}
+
+impl Polygon {
+}
+
+pub struct Circle {
+    // add fields
+}
+
+impl Circle {
+    // add methods
+}
+
+pub enum Shape {
+    Polygon(Polygon),
+    Circle(Circle),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn round_two_digits(x: f64) -> f64 {
+        (x * 100.0).round() / 100.0
+    }
+
+    #[test]
+    fn test_point_magnitude() {
+        let p1 = Point::new(12, 13);
+        assert_eq!(round_two_digits(p1.magnitude()), 17.69);
+    }
+
+    #[test]
+    fn test_point_dist() {
+        let p1 = Point::new(10, 10);
+        let p2 = Point::new(14, 13);
+        assert_eq!(round_two_digits(p1.dist(p2)), 5.00);
+    }
+
+    #[test]
+    fn test_point_add() {
+        let p1 = Point::new(16, 16);
+        let p2 = p1 + Point::new(-4, 3);
+        assert_eq!(p2, Point::new(12, 19));
+    }
+
+    #[test]
+    fn test_polygon_left_most_point() {
+        let p1 = Point::new(12, 13);
+        let p2 = Point::new(16, 16);
+
+        let mut poly = Polygon::new();
+        poly.add_point(p1);
+        poly.add_point(p2);
+        assert_eq!(poly.left_most_point(), Some(p1));
+    }
+
+    #[test]
+    fn test_polygon_iter() {
+        let p1 = Point::new(12, 13);
+        let p2 = Point::new(16, 16);
+
+        let mut poly = Polygon::new();
+        poly.add_point(p1);
+        poly.add_point(p2);
+
+        let points = poly.iter().cloned().collect::<Vec<_>>();
+        assert_eq!(points, vec![Point::new(12, 13), Point::new(16, 16)]);
+    }
+
+    #[test]
+    fn test_shape_perimeters() {
+        let mut poly = Polygon::new();
+        poly.add_point(Point::new(12, 13));
+        poly.add_point(Point::new(17, 11));
+        poly.add_point(Point::new(16, 16));
+        let shapes = vec![
+            Shape::from(poly),
+            Shape::from(Circle::new(Point::new(10, 20), 5)),
+        ];
+        let perimeters = shapes
+            .iter()
+            .map(Shape::perimeter)
+            .map(round_two_digits)
+            .collect::<Vec<_>>();
+        assert_eq!(perimeters, vec![15.48, 31.42]);
+    }
+}
+
+
+
