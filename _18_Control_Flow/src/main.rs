8fc0b8d598663823bb16240ef5bf4f70aacfc9f7
sejin-P <chunpark37@gmail.com>
2023-06-18 00:29:39 +0900

    18.5 while let loops

--- _18_Control_Flow/src/main.rs
+++ _18_Control_Flow/src/main.rs
@@ -15,4 +15,23 @@ fn main() {
         z - y
     };
     println!("x: {x}");
+
+
+
+
+
+
+
+
+
+
+
+
+    // 18.5 while let loops
+    let v = vec![10, 20, 30];
+    let mut iter = v.into_iter();
+
+    while let Some(x) = iter.next() {
+        println!("x: {x}");
+    }
 }
