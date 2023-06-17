226fd2beaa16ceeff47bd2bf98a10f3f951187cc
sejin-P <chunpark37@gmail.com>
2023-06-18 00:31:58 +0900

    18.6 for loops

--- _18_Control_Flow/src/main.rs
+++ _18_Control_Flow/src/main.rs
@@ -34,4 +34,27 @@ fn main() {
     while let Some(x) = iter.next() {
         println!("x: {x}");
     }
+
+    println!("v: {:?}", v);
+
+
+
+
+
+
+
+
+
+
+
+    // 18.6 for loops
+    let v = vec![10, 20, 30];
+
+    for x in v {
+        println!("x: {x}");
+    }
+    
+    for i in (0..10).step_by(2) {
+        println!("i: {i}");
+    }
 }
