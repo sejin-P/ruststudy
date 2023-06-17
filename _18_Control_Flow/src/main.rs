e6151782361435c199e8fb114b0ccc751078928c
sejin-P <chunpark37@gmail.com>
2023-06-18 00:26:42 +0900

    18.1 Blocks

---
+++ _18_Control_Flow/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    // 18.1 Blocks
+    // A block in Rust has a value and a type: the value is the last expression of the block:
+    let x = {
+        let y = 10;
+        println!("y: {y}");
+        let z = {
+            let w = {
+                3 + 4
+            };
+            println!("w: {w}");
+            y * w
+        };
+        println!("z: {z}");
+        z - y
+    };
+    println!("x: {x}");
+}
