f01dbf4e3eb64ed1c4661debd5440dfd79d53a06
sejin-P <chunpark37@gmail.com>
2023-07-01 02:34:45 +0900

    19.3 VEc

--- _19_Standard_Library/src/main.rs
+++ _19_Standard_Library/src/main.rs
@@ -71,4 +71,45 @@ fn main() {
     // To a substring by using s3[0..4], where that slice is on character boundaries or not.
     let s5 = &*s1;
     println!("{}", s5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // 19.3 Vec
+    let mut v1 = Vec::new();
+    v1.push(42);
+    println!("v1: len = {}, capacity = {}", v1.len(), v1.capacity());
+
+    let mut v2 = Vec::with_capacity(v1.len()+1);
+    v2.extend(v1.iter());
+    v2.push(9999);
+    println!("v2: len = {}, capacity = {}", v2.len(), v2.capacity());
+
+    // 이렇게도 assign 가능
+    let mut v3 = vec![0, 0, 1, 2, 3, 4];
+    // retain only the even elements
+    v3.retain(|x| x % 2 == 0);
+    println!("{v3:?}");
+
+    // Remove consecutive duplicates
+    v3.dedup();
+    println!("{v3:?}");
+    //  Vec is a type of collection, along with String and HashMap. The data it contains is stored on the heap. This means the amount of data doesn’t need to be known at compile time. It can grow or shrink at runtime.
+    // Notice how Vec<T> is a generic type too, but you don’t have to specify T explicitly. As always with Rust type inference, the T was established during the first push call.
+    // vec![...] is a canonical macro to use instead of Vec::new() and it supports adding initial elements to the vector.
+    // To index the vector you use [ ], but they will panic if out of bounds. Alternatively, using get will return an Option. The pop function will remove the last element.
+    // Show iterating over a vector and mutating the value: for e in &mut v { *e += 50; }
 }
