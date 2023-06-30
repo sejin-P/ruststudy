6ac9fdee8cce38915b6dc7e08f111d0be8f657fc
sejin-P <chunpark37@gmail.com>
2023-07-01 02:09:43 +0900

    19.2 String

--- _19_Standard_Library/src/main.rs
+++ _19_Standard_Library/src/main.rs
@@ -57,4 +57,16 @@ fn main() {
 
     let s3 = String::from("🇨🇭");
     println!("s3: len = {}, number of chars = {}", s3.len(), s3.chars().count());
+
+    // String::new returns a new empty string, use String::with_capacity when you know how much data you want to push to the string.
+    // String::len returns the size of the String in bytes (which can be different from its length in characters).
+    // String::chars returns an iterator over the actual characters. Note that a char can be different from what a human will consider a “character” due to grapheme clusters.
+    // When people refer to strings they could either be talking about &str or String.
+    // When a type implements Deref<Target = T>, the compiler will let you transparently call methods from T.
+    // String implements Deref<Target = str> which transparently gives it access to str’s methods.
+    // Write and compare let s3 = s1.deref(); and let s3 = &*s1;.
+    // String is implemented as a wrapper around a vector of bytes, many of the operations you see supported on vectors are also supported on String, but with some extra guarantees.
+    // Compare the different ways to index a String:
+    // To a character by using s3.chars().nth(i).unwrap() where i is in-bound, out-of-bounds.
+    // To a substring by using s3[0..4], where that slice is on character boundaries or not.
 }
