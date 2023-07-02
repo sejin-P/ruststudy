0378272fc5c8ec3e5b78be5426fddf5f651c74f9
sejin-P <chunpark37@gmail.com>
2023-07-02 23:22:31 +0900

    19.6 RC

--- _19_Standard_Library/src/main.rs
+++ _19_Standard_Library/src/main.rs
@@ -1,4 +1,6 @@
 use std::collections::HashMap;
+use std::rc::{Rc, Weak};
+use std::cell::RefCell;
 fn main() {
     // Option and Result types: used for optional values and error handling.
 
@@ -300,4 +302,66 @@ fn main() {
     // 19.5.2 Niche Optimization
     // A Box cannot be empty, so the pointer is always valid and non-null. This allows the compiler to optimize the memory layout.
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // 19.6 Rc
+    // Rc is a reference-counted shared pointer. Use this when you need to refer to the same data from multiple places:
+    let mut a = Rc::new(10);
+    let mut b = Rc::clone(&a);
+
+    println!("a: {a}");
+    println!("b: {b}");
+    // If you need to mutate the data inside an Rc, you will need to wrap the data in a type such as Cell or RefCell.
+    // See Arc if you are in a multi-threaded context.
+    // You can downgrade a shared pointer into a Weak pointer to create cycles that will get dropped.
+
+    // Rc’s count ensures that its contained value is valid for as long as there are references.
+    // Like C++’s std::shared_ptr.
+    // Rc::clone is cheap: it creates a pointer to the same allocation and increases the reference count. Does not make a deep clone and can generally be ignored when looking for performance issues in code.
+    // make_mut actually clones the inner value if necessary (“clone-on-write”) and returns a mutable reference.
+    // Use Rc::strong_count to check the reference count.
+    // Compare the different datatypes mentioned. Box enables (im)mutable borrows that are enforced at compile time. RefCell enables (im)mutable borrows that are enforced at run time and will panic if it fails at runtime.
+    // Rc::downgrade gives you a weakly reference-counted object to create cycles that will be dropped properly (likely in combination with RefCell).
+
+    #[derive(Debug)]
+    struct Node {
+        value: i64,
+        parent: Option<Weak<RefCell<Node>>>,
+        children: Vec<Rc<RefCell<Node>>>,
+    }
+
+    let mut root = Rc::new(RefCell::new(Node {
+        value: 42,
+        parent: None,
+        children: vec![],
+    }));
+
+    let child = Rc::new(RefCell::new(Node {
+        value: 43,
+        children: vec![],
+        parent: Some(Rc::downgrade(&root))
+    }));
+    root.borrow_mut().children.push(child);
+    println!("graph: {root:#?}");
 }
