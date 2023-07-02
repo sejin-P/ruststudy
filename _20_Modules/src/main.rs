aebd07ee70b8a0692363255d5decac672aa81537
sejin-P <chunpark37@gmail.com>
2023-07-02 23:54:49 +0900

    20.1 Visibility

--- _20_Modules/src/main.rs
+++ _20_Modules/src/main.rs
@@ -14,4 +14,48 @@ fn main() {
 
     foo::do_something();
     bar::do_something();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // 20.1 Visibility
+    // Modules are a privacy boundary:
+
+    // Module items are private by default (hides implementation details).
+    // Parent and sibling items are always visible.
+    // In other words, if an item is visible in module foo, it’s visible in all the descendants of foo.
+
+    mod outer {
+        fn private() {
+            println!("outer::private");
+        }
+
+        pub fn public() {
+            println!("outer::public");
+        }
+
+        mod inner {
+            fn private() {
+                println!("outer::inner::private");
+            }
+
+            pub fn public() {
+                println!("outer::inner::public");
+                super::private();
+            }
+        }
+    }
+
+    outer::public();
 }
