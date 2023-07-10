22283d8e4d98f8e3e37398a97cabd7344925d4d1
sejin-P <chunpark37@gmail.com>
2023-07-10 21:42:18 +0900

    23.2 Generic Methods

--- _23_Generics/src/main.rs
+++ _23_Generics/src/main.rs
@@ -23,5 +23,26 @@ fn main() {
 
 
 
+    // 23.2 Generic Methods
+    #[derive(Debug)]
+    struct Point1<T>(T, T);
 
+    impl<T> Point1<T> {
+        fn x(&self) -> &T {
+            &self.0
+        }
+
+        fn set_x(&mut self, x:T) {
+            
+        }
+    }
+
+    let p = Point1(5, 10);
+    println!("p.x = {}", p.x());
+
+    // Q: Why T is specified twice in impl<T> Point<T> {}? Isn’t that redundant?
+    // This is because it is a generic implementation section for generic type. They are independently generic.
+    // It means these methods are defined for any T.
+    // It is possible to write impl Point<u32> { .. }.
+    // Point is still generic and you can use Point<f64>, but methods in this block will only be available for Point<u32>.
 }
