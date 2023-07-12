3521053e905c4b875330a470b1d36ebc9fe08092
sejin-P <chunpark37@gmail.com>
2023-07-12 23:06:30 +0900

    24.1 Trait Objects

--- _24_Traits/src/main.rs
+++ _24_Traits/src/main.rs
@@ -33,4 +33,62 @@ fn main() {
 
     let captain_floof = Cat;
     greet(&captain_floof);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // 24.1 Trait Objects
+    trait Pet1 {
+        fn name(&self) -> String;
+    }
+
+    struct Dog1 {
+        name: String,
+    }
+
+    struct Cat1;
+
+    impl Pet1 for Dog1 {
+        fn name(&self) -> String {
+            self.name.clone()
+        }
+    }
+
+    impl Pet1 for Cat1 {
+        fn name(&self) -> String {
+            String::from("The cat")
+        }
+    }
+
+    let pets: Vec<Box<dyn Pet1>> = vec![
+        Box::new(Cat1),
+        Box::new(Dog1 { name: String::from("Fido") }),
+    ];
+
+    for pet in pets {
+        println!("Hello {}!", pet.name());
+    }
+
+    // Types that implement a given trait may be of different sizes. This makes it impossible to have things like Vec<Pet> in the example above.
+    // dyn Pet is a way to tell the compiler about a dynamically sized type that implements Pet.
+    // In the example, pets holds fat pointers to objects that implement Pet. The fat pointer consists of two components, a pointer to the actual object and a pointer to the virtual method table for the Pet implementation of that particular object.
+    // Compare these outputs in the above example:
+
+    println!("{} {}", std::mem::size_of::<Dog1>(), std::mem::size_of::<Cat1>());
+    println!("{} {}", std::mem::size_of::<&Dog1>(), std::mem::size_of::<&Cat1>());
+    println!("{}", std::mem::size_of::<&dyn Pet1>());
+    println!("{}", std::mem::size_of::<Box<dyn Pet1>>());
 }
