b70a3295259c6d050af89d5f3adb07cf5b69abd3
sejin-P <chunpark37@gmail.com>
2023-07-14 21:54:39 +0900

    25.2 FromIterator

--- _25_Important_Traits/src/main.rs
+++ _25_Important_Traits/src/main.rs
@@ -20,4 +20,42 @@ fn main() {
     for (i, n) in fib.enumerate().take(5) {
         println!("fib({i}): {n}");
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // 25.2 FromIterator
+    let primes = vec![2, 3, 5, 7];
+    let prime_squres = primes
+        .into_iter()
+        .map(|prime| prime * prime)
+        .collect::<Vec<_>>();
+
+    // Iterator implements fn collect<B>(self) -> B where B: FromIterator<Self::Item>, Self: Sized
+
+    // There are also implementations which let you do cool things like convert an Iterator<Item = Result<V, E>> into a Result<Vec<V>, E>.
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
 }
