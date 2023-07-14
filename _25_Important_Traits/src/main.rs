a22bb5eaa14d851cd12e7ef1f25a133b298130f9
sejin-P <chunpark37@gmail.com>
2023-07-14 22:18:32 +0900

    25.4 Read and Write

--- _25_Important_Traits/src/main.rs
+++ _25_Important_Traits/src/main.rs
@@ -1,3 +1,5 @@
+use std::io::{BufRead, BufReader, Read, Result, Write};
+
 fn main() {
     // 25.1 Iterators
 
@@ -76,4 +78,48 @@ fn main() {
 
     // That’s why it is common to only implement From, as your type will get Into implementation too.
     // When declaring a function argument input type like “anything that can be converted into a String”, the rule is opposite, you should use Into. Your function will accept types that implement From and those that only implement Into.
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // 25.4 Read and Write
+    fn count_lines<R: Read>(reader: R) -> usize {
+        let buf_reader = BufReader::new(reader);
+        buf_reader.lines().count()
+    }
+
+    let slice: &[u8] = b"foo]\nbar\nbaz\n";
+    println!("lines in slice: {}", count_lines(slice));
+
+    fn exe() -> Result<()> {
+        let file = std::fs::File::open(std::env::current_exe()?)?;
+        println!("lines in file: {}", count_lines(file));
+        Ok(())
+    }
+    
+    exe();
+
+    fn log<W: Write>(writer: &mut W, msg: &str) -> Result<()> {
+        writer.write_all(msg.as_bytes());
+        writer.write_all("\n".as_bytes())
+    }
+
+    fn wexe() -> Result<()> {
+        let mut buffer = Vec::new();
+        log(&mut buffer, "Hello")?;
+        log(&mut buffer, "World")?;
+        println!("Logged: {:?}", buffer);
+        Ok(())
+    }
+
+    wexe();
 }
