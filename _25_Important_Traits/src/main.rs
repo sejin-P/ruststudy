0578ce68d891f9167edfdb4c53bff4a59ae77a1b
sejin-P <chunpark37@gmail.com>
2023-07-14 21:58:24 +0900

    25.3 From and Into

--- _25_Important_Traits/src/main.rs
+++ _25_Important_Traits/src/main.rs
@@ -57,5 +57,23 @@ fn main() {
 
 
     
+    // 25.3 From and Into
 
+    // Types implement From and Into to facilitate type conversions:
+    let s = String::from("hello");
+    let addr = std::net::Ipv4Addr::from([127 , 0, 0, 1]);
+    let one = i16::from(true);
+    let bigger = i16::from(123i16);
+
+    println!("{s}, {addr}, {one}, {bigger}");
+
+    // Into is automatically implemented when From is implemented:
+    let s: String = "hello".into();
+    let addr: std::net::Ipv4Addr = [127, 0, 0, 1].into();
+    let one:i16 = true.into();
+    let bigger: i32 = 123i16.into();
+    println!("{s}, {addr}, {one}, {bigger}");
+
+    // That’s why it is common to only implement From, as your type will get Into implementation too.
+    // When declaring a function argument input type like “anything that can be converted into a String”, the rule is opposite, you should use Into. Your function will accept types that implement From and those that only implement Into.
 }
