09a5cdcc344fc2f9ed103b6be32f443be578ccf2
sejin-P <chunpark37@gmail.com>
2023-07-15 15:10:22 +0900

    25.8 Closures

--- _25_Important_Traits/src/main.rs
+++ _25_Important_Traits/src/main.rs
@@ -244,6 +244,10 @@ fn main() {
 
 
 
+    // Copy 구현시 Clone은 항상 따라와야한다 -> 언어상의 consistency와 confusion 방지를 위해
+
+
+
 
 
 
@@ -251,4 +255,26 @@ fn main() {
 
 
     
+
+    // 25.8 Closures
+    // Closures or lambda expressions have types which cannot be named. However, they implement special Fn, FnMut, and FnOnce traits
+    fn apply_with_log(func: impl FnOnce(i32) -> i32, input: i32) -> i32 {
+        println!("Calling function on {input}");
+        func(input)
+    }
+
+    let add_3 = |x| x + 3;
+    println!("add_3: {}", apply_with_log(add_3, 10));
+    println!("add_3: {}", apply_with_log(add_3, 20));
+
+    let mut v: Vec<i32> = Vec::new();
+    let mut accumulate = |x| {
+        v.push(x);
+        return v.iter().sum::<i32>()
+    };
+    println!("accumulate: {}", apply_with_log(&mut accumulate, 4));
+    println!("accumulate: {}", apply_with_log(&mut accumulate, 5));
+
+    let multiply_sum = |x| x * v.into_iter().sum::<i32>();
+    println!("multiply_sum: {}", apply_with_log(multiply_sum, 3));
 }
