f3102c56cb9ff27dab65c4bf7a7190a46d97c8bd
sejin-P <chunpark37@gmail.com>
2023-07-16 22:58:43 +0900

    27.1.1 Catching the Stack Unwinding

--- _27_Error_Handling/src/main.rs
+++ _27_Error_Handling/src/main.rs
@@ -1,9 +1,30 @@
+use std::panic;
+
 fn main() {
     // 27.1 Panics
     let v = vec![10, 20, 30];
-    println!("v[100]: {}", v[100]);
+    // println!("v[100]: {}", v[100]);
 
     // Panics are for unrecoverable and unexpected errors.
     // Panics are symptoms of bugs in the program.
     // Use non-panicking APIs(such as Vec::get) if crashing is not acceptable
+
+
+    
+
+    // 27.1.1 Catching and Stack Unwinding
+    let result = panic::catch_unwind(|| {
+        println!("hello");
+    });
+
+    assert!(result.is_ok());
+
+    let result = panic::catch_unwind(|| {
+        panic!("oh no!");
+    });
+
+    assert!(result.is_err());
+
+    // This can be useful in servers which should keep running even if a single request crashes.
+    // This does not work if panic = 'abort' is set in your Cargo.toml.
 }
