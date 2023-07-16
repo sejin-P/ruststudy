9557b0a7b0ca9f02371ef907f7693854911f8775
sejin-P <chunpark37@gmail.com>
2023-07-16 23:02:54 +0900

    27.2 Structured Error Handling

--- _27_Error_Handling/src/main.rs
+++ _27_Error_Handling/src/main.rs
@@ -1,4 +1,6 @@
 use std::panic;
+use std::fs;
+use std::io::Read;
 
 fn main() {
     // 27.1 Panics
@@ -28,4 +30,34 @@ fn main() {
 
     // This can be useful in servers which should keep running even if a single request crashes.
     // This does not work if panic = 'abort' is set in your Cargo.toml.
+
+
+
+
+
+
+
+
+
+
+    // 27.2 Structured Error Handling With Result'
+    // We have already seen the Result enum. This is used pervasively when errors are expected as part of normal operation:
+    let file = fs::File::open("dairy.txt");
+    match file {
+        Ok(mut file) => {
+            let mut contents = String::new();
+            file.read_to_string(&mut contents);
+            println!("Dear diary: {contents}");
+        },
+        Err(err) => {
+            println!("The diary could not be opened: {err}");
+        }
+    }
+
+
+    // As with Option, the successful value sits inside of Result, forcing the developer to explicitly extract it. This encourages error checking. In the case where an error should never happen, unwrap() or expect() can be called, and this is a signal of the developer intent too.
+    // Result documentation is a recommended read. Not during the course, but it is worth mentioning. It contains a lot of convenience methods and functions that help functional-style programming.
+
+
+
 }
