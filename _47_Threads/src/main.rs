3bb0fe6e00358645ac0a7d6e93b11880be568001
sejin-P <chunpark37@gmail.com>
2023-07-24 00:36:59 +0900

    47.1 Scoped Threads

--- _47_Threads/src/main.rs
+++ _47_Threads/src/main.rs
@@ -1,4 +1,5 @@
 use std::thread;
+use std::thread::scope;
 use std::time::Duration;
 
 fn main() {
@@ -34,4 +35,29 @@ fn main() {
     handle.join().expect("should wait this thread");
     println!("i waited!");
 
+
+
+
+
+
+
+
+
+    // 47.1 Scoped Threads
+    // Normal threads cannot borrow from their environment:
+    let s = String::from("Hello");
+    // thread::spawn(|| {
+    //     println!("Length: {}", s.len());
+    // });
+
+    thread::scope(|scope| {
+        scope.spawn(|| {
+            println!("Length: {}", s.len());
+        });
+    });
+
+    // The reason for that is that when the thread::scope function completes, all the threads are
+    // guaranteed to be joined, so they can return borrowed data.
+    // Normal Rust borrowing rules apply: you can either borrow mutably by one thread, or immutably
+    // by any number of threads.
 }
