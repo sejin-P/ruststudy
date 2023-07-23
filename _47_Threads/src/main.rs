5b14d4d444c9c04a873806586b5886cca1dcfa9e
sejin-P <chunpark37@gmail.com>
2023-07-24 01:36:25 +0900

    50.3 Example

--- _47_Threads/src/main.rs
+++ _47_Threads/src/main.rs
@@ -258,4 +258,39 @@ fn main() {
     // Why does lock() return a Result?
     // If the thread that held the Mutex panicked, the Mutex becomes “poisoned” to signal that the
     // data it protected might be in an inconsistent state. Calling lock() on a poisoned mutex fails with a PoisonError. You can call into_inner() on the error to recover the data regardless.
+
+
+
+
+
+
+
+    // 50.3 Example
+    // let mut v = vec![10, 20, 30];
+    // let handle = thread::spawn(|| {
+    //     v.push(10);
+    // });
+    // v.push(1000);
+
+    let v = Arc::new(Mutex::new(vec![10, 20, 30]));
+
+    let v2 = Arc::clone(&v);
+    let handle = thread::spawn(move || {
+        let mut v2 = v2.lock().unwrap();
+        v2.push(10);
+    });
+
+    // println!("{v2:?}");
+
+    {
+        let mut v = v.lock().unwrap();
+        v.push(1000);
+    }
+
+    handle.join().unwrap();
+    println!("v: {v:?}");
+    // v is wrapped in both Arc and Mutex, because their concerns are orthogonal.
+    // Wrapping a Mutex in an Arc is a common pattern to share mutable state between threads.
+    // v: Arc<_> needs to be cloned as v2 before it can be moved into another thread. Note move was added to the lambda signature.
+    // Blocks are introduced to narrow the scope of the LockGuard as much as possible.
 }
