385fc6006d3e10653b91d9f90c6b99baaa6d1f1e
sejin-P <chunpark37@gmail.com>
2023-07-23 21:26:31 +0900

    28.2 Test Modules

--- _28_Testing/src/main.rs
+++ _28_Testing/src/main.rs
@@ -1,3 +1,4 @@
+use std::fmt::format;
 
 // 28.1 Unit Tests
 fn first_word(text: &str) -> &str {
@@ -22,6 +23,33 @@ fn test_multiple_words() {
     assert_eq!(first_word("Hello World"), "Hello");
 }
 
+// 28.2 Test Modules
+// Unit tests are often put in a nestued module.
+
+fn helper(a: &str, b: &str) -> String {
+    format!("{a} {b}")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_helper() {
+        assert_eq!(helper("foo", "bar"), "foo bar");
+    }
+}
+
+// This lets you unit test private helpers
+// The #[cfg(test)] attribute is only active when you run cargo test
+
+
+
+
+
+//
+
 fn main() {
     println!("Hello, world!");
 }
+
