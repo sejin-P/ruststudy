9466a1716f22d8345e969a950d0ce72b289ad3e9
sejin-P <chunpark37@gmail.com>
2023-07-24 01:16:45 +0900

    50.1 Arc

--- _47_Threads/src/main.rs
+++ _47_Threads/src/main.rs
@@ -1,4 +1,4 @@
-use std::sync::mpsc;
+use std::sync::{Arc, mpsc};
 use std::thread;
 use std::thread::scope;
 use std::time::Duration;
@@ -204,4 +204,32 @@ fn main() {
     // T when the last reference is dropped.
 
     // `Mutex<T>`: ensures mutually exclusive access to the T value.
+
+
+
+
+
+
+
+
+    // 50.1 Arc
+    // `Arc<T>` allows shared read-only access via `Arc::clone`:
+    let v = Arc::new(vec![10, 20, 30]);
+    let mut handles = Vec::new();
+    for _ in 1..5 {
+        let v = Arc::clone(&v);
+        handles.push(thread::spawn(move || {
+            let thread_id = thread::current().id();
+            println!("{thread_id:?}: {v:?}");
+        }));
+    }
+
+    handles.into_iter().for_each(|h| h.join().unwrap());
+    println!("v: {v:?}");
+
+    // Arc stands for “Atomic Reference Counted”, a thread safe version of Rc that uses atomic operations.
+    // Arc<T> implements Clone whether or not T does. It implements Send and Sync iff T implements them both.
+    // Arc::clone() has the cost of atomic operations that get executed, but after that the use of the T is free.
+    // Beware of reference cycles, Arc does not use a garbage collector to detect them.
+    // std::sync::Weak can help.
 }
