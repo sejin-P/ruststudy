4d943af8bb4a3aa45f96f3f7bc7d7b94d702cf42
sejin-P <chunpark37@gmail.com>
2023-07-24 01:30:54 +0900

    50.2 Mutex

--- _47_Threads/src/main.rs
+++ _47_Threads/src/main.rs
@@ -1,4 +1,4 @@
-use std::sync::{Arc, mpsc};
+use std::sync::{Arc, mpsc, Mutex};
 use std::thread;
 use std::thread::scope;
 use std::time::Duration;
@@ -232,4 +232,30 @@ fn main() {
     // Arc::clone() has the cost of atomic operations that get executed, but after that the use of the T is free.
     // Beware of reference cycles, Arc does not use a garbage collector to detect them.
     // std::sync::Weak can help.
+
+
+
+
+
+
+    // 50.2 Mutex
+    // `Mutex<T>` ensures mutual exclusion and allows mutable access to T behind a read-only interface
+
+    let v = Mutex::new(vec![10, 20, 30]);
+    println!("v: {:?}", v.lock().unwrap());
+    {
+        let mut guard = v.lock().unwrap();
+        guard.push(40);
+    }
+
+    println!("v: {:?}", v.lock().unwrap());
+
+    // Mutex in Rust looks like a collection with just one element - the protected data.
+    // It is not possible to forget to acquire the mutex before accessing the protected data.
+    // You can get an &mut T from an &Mutex<T> by taking the lock. The MutexGuard ensures that the &mut T doesn’t outlive the lock being held.
+    // Mutex<T> implements both Send and Sync iff (if and only if) T implements Send.
+    // A read-write lock counterpart - RwLock.
+    // Why does lock() return a Result?
+    // If the thread that held the Mutex panicked, the Mutex becomes “poisoned” to signal that the
+    // data it protected might be in an inconsistent state. Calling lock() on a poisoned mutex fails with a PoisonError. You can call into_inner() on the error to recover the data regardless.
 }
