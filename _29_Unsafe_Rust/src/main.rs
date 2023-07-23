d8fb4db942c7a981208416b224c7ec1d7e14367e
sejin-P <chunpark37@gmail.com>
2023-07-23 23:02:46 +0900

    29.5 Implementing Unsafe Traits

--- _29_Unsafe_Rust/src/main.rs
+++ _29_Unsafe_Rust/src/main.rs
@@ -1,3 +1,6 @@
+use std::mem::size_of_val;
+use std::slice;
+
 fn main() {
     // 29.0 Unsafe Rust
     // Safe Rust: memory safe, no undefined behavior possible.
@@ -170,4 +173,36 @@ fn main() {
     // This is usually only a problem for extern functions which do things with pointers which might
     // violate Rust's memory model, but in general any C function might have undefined behavior under
     // any arbitrary circumstances.
+
+
+
+
+
+
+
+
+
+    // 29.5 Implementing Unsafe Traits
+    // Like with functions, you can mark a trait as `unsafe` if the implementation must guarantee
+    // particular conditions to avoid undefined behavior.
+
+    // For example, the `zerocopy` crate has an `unsafe` trait that looks something like this: https://docs.rs/zerocopy/latest/zerocopy/trait.AsBytes.html
+    /// # Safety
+    /// The type must have a defined representation and no padding
+    pub unsafe trait AsBytes {
+        fn as_bytes(&self) -> &[u8] {
+            unsafe {
+                slice::from_raw_parts(self as *const Self as *const u8, size_of_val(self))
+            }
+        }
+    }
+
+    // Safe because u32 has a defined representation and no padding.
+    unsafe impl AsBytes for u32 {}
+
+    // There should be a # Safety section on the Rustdoc for the trait explaining the requirements for the trait to be safely implemented.
+    //
+    // The actual safety section for AsBytes is rather longer and more complicated.
+    //
+    // The built-in Send and Sync traits are unsafe.
 }
