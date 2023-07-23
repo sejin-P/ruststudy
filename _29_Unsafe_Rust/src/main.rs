e22d15b4ad37c34eb0c5cdbb133550c9d58428c0
sejin-P <chunpark37@gmail.com>
2023-07-23 22:48:53 +0900

    29.3 Unions

--- _29_Unsafe_Rust/src/main.rs
+++ _29_Unsafe_Rust/src/main.rs
@@ -63,4 +63,27 @@ fn main() {
 
     // Using a mutable static is generally a bad idea, but there are some cases where it might make sense
     // in low-level `no_std` code, such as implementing a heap allocator or working with some C APIs.
+
+
+
+
+
+
+    // 29.3 Unions
+    // Unions are like enums, but you need to track the active field yourslef:
+
+    #[repr(C)]
+    union MyUnion {
+        i: u8,
+        b: bool,
+    }
+
+    let u = MyUnion { i: 42 };
+    println!("int: {}", unsafe { u.i });
+    println!("bool: {}", unsafe { u.b }); // Undefined behavior!
+
+    // Unions are very rarely needed in Rust as you can usually use an enum. They are occasionally needed
+    // for interacting with C libirary APIs.
+    // If you just want to reinterpret bytes as a different type, you probably want `std::mem::transmute`
+    // or a safe wrapper such as the `zerocopy` crate.
 }
