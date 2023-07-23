6ed7448fe33f1cb749f22565965c4398111c410c
sejin-P <chunpark37@gmail.com>
2023-07-23 22:53:32 +0900

    29.4 Writing Unsafe Functions

--- _29_Unsafe_Rust/src/main.rs
+++ _29_Unsafe_Rust/src/main.rs
@@ -86,4 +86,38 @@ fn main() {
     // for interacting with C libirary APIs.
     // If you just want to reinterpret bytes as a different type, you probably want `std::mem::transmute`
     // or a safe wrapper such as the `zerocopy` crate.
+
+
+
+
+
+
+
+
+
+
+
+    // 29.4 Calling Unsafe Functions
+    // A function or method can be marked `unsafe` if it has extra preconditions you must uphold to
+    // avoid undefined behavior:
+    let emojis = "🗻∈🌏";
+    // Safe because the indices are in the correct order, within the bounds of
+    // the string slice, and lie on UTF-8 sequence boundaries.
+    unsafe {
+        println!("emoji: {}", emojis.get_unchecked(0..4));
+        println!("emoji: {}", emojis.get_unchecked(4..7));
+        println!("emoji: {}", emojis.get_unchecked(7..11));
+    }
+
+    fn count_chars(s: &str) -> usize {
+        s.chars().map(|_| 1).sum()
+    }
+    println!("char count: {}", count_chars(unsafe { emojis.get_unchecked(0..7) }));
+
+    // Not upholding the UTF-8 encoding requirement breaks memory safety!
+    println!("emoji: {}", unsafe { emojis.get_unchecked(0..3) });
+    println!("char count: {}", count_chars(unsafe { emojis.get_unchecked(0..3) }));
+
+
+
 }
