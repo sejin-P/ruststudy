4599dc50d6c4c54ac9aac7fc87e5b5efca855b08
sejin-P <chunpark37@gmail.com>
2023-07-23 22:19:41 +0900

    29.0 Unsafe Rust

---
+++ _29_Unsafe_Rust/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // 29.0 Unsafe Rust
+    // Safe Rust: memory safe, no undefined behavior possible.
+    // Unsafe Rust: can trigger undefined behavior if preconditions are violated.
+    // 1. Dereference raw pointers.
+    // 2. Access or modify mutable static variables.
+    // 3. Access union fields.
+    // 4. Call unsafe functions, including extern functions.
+    // 5. Implement unsafe traits.
+}
