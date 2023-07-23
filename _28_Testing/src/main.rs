c66e523a3f3f429db30a352e285f2175f23e48b1
sejin-P <chunpark37@gmail.com>
2023-07-23 21:22:51 +0900

    28.1 Unit Tests

---
+++ _28_Testing/src/main.rs
@@ -0,0 +1,27 @@
+
+// 28.1 Unit Tests
+fn first_word(text: &str) -> &str {
+    match text.find(' ') {
+        Some(idx) => &text[..idx],
+        None => &text,
+    }
+}
+
+#[test]
+fn test_empty() {
+    assert_eq!(first_word(""), "");
+}
+
+#[test]
+fn test_single_word() {
+    assert_eq!(first_word("Hello"), "Hello");
+}
+
+#[test]
+fn test_multiple_words() {
+    assert_eq!(first_word("Hello World"), "Hello");
+}
+
+fn main() {
+    println!("Hello, world!");
+}
