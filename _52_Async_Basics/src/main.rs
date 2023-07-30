1257ae8b947d988aab3d9bce854718010e05bb1e
sejin-P <chunpark37@gmail.com>
2023-07-30 23:02:20 +0900

    52.1 Async/Await

--- _52_Async_Basics/src/main.rs
+++ _52_Async_Basics/src/main.rs
@@ -1,3 +1,15 @@
+use futures::executor::block_on;
+
+async fn count_to(count: i32) {
+    for i in 1..=count {
+        println!("Count is: {i}");
+    }
+}
+
+async fn async_main(count: i32) {
+    count_to(count).await;
+}
+
 fn main() {
     // “Async” is a concurrency model where multiple tasks are executed concurrently by executing each
     // task until it would block, then switching to another task that is ready to make progress.
@@ -12,4 +24,20 @@ fn main() {
 
     // Python's asyncio and js's Promise are similar to Rust's future, but they are callback-based.
 
+
+
+
+
+
+    // 52.1 Async/Await
+    block_on(async_main(10));
+
+    let future: () = async_main(10);
+
+    // You cannot make main async, without additional instructions to the compiler on how to use the returned future.
+    // You need an executor to run async code. block_on blocks the current thread until the provided
+    // future has run to completion.
+    // `.await` asynchronously waits for the completion of another operation. Unlike block_on,
+    // `.await` doesn’t block the current thread.
+    // .await can only be used inside an async function (or block; these are introduced later).
 }
