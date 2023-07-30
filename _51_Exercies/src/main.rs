cbdb25b4adef2bc89318314a0ef9c843dc6e3cd4
sejin-P <chunpark37@gmail.com>
2023-07-30 23:55:09 +0900

    fix; deadlock

--- _51_Exercies/src/main.rs
+++ _51_Exercies/src/main.rs
@@ -31,6 +31,36 @@ impl Philosopher {
     }
 }
 
+// struct Philosopher {
+//     name: String,
+//     // ANCHOR_END: Philosopher
+//     left_fork: Arc<Mutex<Fork>>,
+//     right_fork: Arc<Mutex<Fork>>,
+//     thoughts: mpsc::SyncSender<String>,
+// }
+//
+// // ANCHOR: Philosopher-think
+// impl Philosopher {
+//     fn think(&self) {
+//         self.thoughts
+//             .send(format!("Eureka! {} has a new idea!", &self.name))
+//             .unwrap();
+//     }
+//     // ANCHOR_END: Philosopher-think
+//
+//     // ANCHOR: Philosopher-eat
+//     fn eat(&self) {
+//         // ANCHOR_END: Philosopher-eat
+//         println!("{} is trying to eat", &self.name);
+//         let left = self.left_fork.lock().unwrap();
+//         let right = self.right_fork.lock().unwrap();
+//
+//         // ANCHOR: Philosopher-eat-end
+//         println!("{} is eating...", &self.name);
+//         thread::sleep(Duration::from_millis(10));
+//     }
+// }
+
 static PHILOSOPHERS: &[&str] =
     &["Socrates", "Plato", "Aristotle", "Thales", "Pythagoras"];
 
@@ -57,8 +87,12 @@ fn main() {
             right_fork: None,
             thoughts: tx,
         };
-        let l = forks[idx].clone();
-        let r = forks[(idx+1)%5].clone();
+        let mut l = Arc::clone(&forks[idx]);
+        let mut r = Arc::clone(&forks[(idx+1)%5]);
+
+        if idx == 4 {
+            std::mem::swap(&mut l, &mut r);
+        }
         thread::spawn(move || {
             for i in 0..100 {
                 p.think();
@@ -69,7 +103,47 @@ fn main() {
     // Output their thoughts
 
     drop(tx);
-    for thought in rx.iter() {
+    for thought in rx {
+        println!("thought");
         println!("{thought:?}");
     }
+
+    // ANCHOR_END: Philosopher-eat-end
+    // let (tx, rx) = mpsc::sync_channel(10);
+    //
+    // let forks = (0..PHILOSOPHERS.len())
+    //     .map(|_| Arc::new(Mutex::new(Fork)))
+    //     .collect::<Vec<_>>();
+    //
+    // for i in 0..forks.len() {
+    //     let tx = tx.clone();
+    //     let mut left_fork = Arc::clone(&forks[i]);
+    //     let mut right_fork = Arc::clone(&forks[(i + 1) % forks.len()]);
+    //
+    //     // To avoid a deadlock, we have to break the symmetry
+    //     // somewhere. This will swap the forks without deinitializing
+    //     // either of them.
+    //     if i == forks.len() - 1 {
+    //         std::mem::swap(&mut left_fork, &mut right_fork);
+    //     }
+    //
+    //     let philosopher = Philosopher {
+    //         name: PHILOSOPHERS[i].to_string(),
+    //         thoughts: tx,
+    //         left_fork,
+    //         right_fork,
+    //     };
+    //
+    //     thread::spawn(move || {
+    //         for _ in 0..100 {
+    //             philosopher.eat();
+    //             philosopher.think();
+    //         }
+    //     });
+    // }
+    //
+    // drop(tx);
+    // for thought in rx {
+    //     println!("{thought}");
+    // }
 }
